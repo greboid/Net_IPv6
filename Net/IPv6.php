d44c88b2ffa08b2eae2f9fed9d8b61af66cfef19
allandanton <33146694+allandanton@users.noreply.github.com>
2021-10-30 16:36:59 +0200

    PHP 8 compatibility

--- Net/IPv6.php
+++ Net/IPv6.php
@@ -853,10 +853,10 @@ class Net_IPv6
                 return array("", $ip);
             }
 
-            $ip{$pos} = '_';
+            $ip[$pos] = '_';
             $ipPart   = explode('_', $ip);
             
-            if ($ip{$pos-1} === ":") {
+            if ($ip[$pos-1] === ":") {
                 $ipPart[0] .= ":";
             }
 
